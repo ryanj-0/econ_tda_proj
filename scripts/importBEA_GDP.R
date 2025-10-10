1e2b1fe4c3da5c99b459efb2aa96788d570658d4
ryanj-0 <johnson.ryan1019@gmail.com>
2025-10-09 21:55:19 -0600

    change name separate data catagories

---
+++ scripts/importBEA_GDP.R
@@ -0,0 +1,49 @@
+########################################################
+## Loading Data From Beureau of Economic Analysis (BEA):
+## National Income and Product Accounts (NIPA)
+## - GDP
+########################################################
+
+# Global config for NIPA Tables
+beaConfig <- list('UserID' = my_beaKey,
+                  'Method' = 'GetData',
+                  'Year' = 'ALL',    # Get All Years Available
+                  'ResultFormat' = 'json')
+
+# Annual GDP
+gdpA_list <- append(beaConfig, list('datasetname' = 'NIPA',
+                                    'TableName' = 'T10101',
+                                    'Frequency' = 'A'))
+
+GDP_A <- beaGet(gdpA_list) |>
+    pivot_longer(cols = !(TableName:UNIT_MULT),
+                 names_to = "time",
+                 values_to = "pctChange") |>
+    mutate(year = str_extract(time, "[0-9]{4}"),
+           LineDescription = case_when(LineNumber == 17 ~ "ExportGoods",
+                                       LineNumber == 18 ~ "ExportServices",
+                                       LineNumber == 20 ~ "ImportGoods",
+                                       LineNumber == 21 ~ "ImportServices",
+                                       .default = LineDescription)) |>
+    select(LineDescription, CL_UNIT, year, pctChange) |>
+    pivot_wider(names_from = LineDescription,
+                values_from = pctChange)
+
+# GDP Quartly
+gdpQ_list <- append(beaConfig, list('datasetname' = 'NIPA',
+                                    'TableName' = 'T10101',
+                                    'Frequency' = 'Q'))
+GDP_Q <- beaGet(gdpQ_list) |>
+    pivot_longer(cols = !(TableName:UNIT_MULT),
+                 names_to = "time",
+                 values_to = "pctChange") |>
+    mutate(year = str_extract(time, "[0-9]{4}",),
+           quarter = str_extract(time, "\\d$"),
+           LineDescription = case_when(LineNumber == 17 ~ "ExportGoods",
+                                       LineNumber == 18 ~ "ExportServices",
+                                       LineNumber == 20 ~ "ImportGoods",
+                                       LineNumber == 21 ~ "ImportServices",
+                                       .default = LineDescription)) |>
+    select(LineDescription, CL_UNIT, year, quarter, pctChange) |>
+    pivot_wider(names_from = LineDescription,
+                values_from = pctChange)
