ddc8475f69a9f35e40c95d24f11f96d242763d40
ryanj-0 <johnson.ryan1019@gmail.com>
2025-10-21 23:05:26 -0600

    update formatting

--- scripts/systemConfig.R
+++ scripts/systemConfig.R
@@ -6,36 +6,31 @@
 rm(list = ls())
 
 # Load Packages -----------------------------------------------------------
-if(!require("pacman")){
+if (!require("pacman")) {
     install.packages("pacman")
 }
 
 # Will install following packages if not already installed
-pacman::p_load(
-    tidyverse,
-    bea.R,
-    fredr,
-    BallMapper,
-    viridis,
-    knitr,
-    kableExtra,
-    svglite
-)
+pacman::p_load(tidyverse,
+               bea.R,
+               fredr,
+               BallMapper,
+               viridis,
+               knitr,
+               kableExtra,
+               svglite)
 
 
 # Source Functions --------------------------------------------------------
 
-fxnList <-
-    list.files(paste(getwd(), "functions", sep = "/"),
-               full.names = TRUE)
+fxnList <- list.files(paste(getwd(), "functions", sep = "/"), full.names = TRUE)
 
 lapply(fxnList, source) |> invisible()
 
 # API Keys ------------------------------------------------------------
 
 # Load API Keys
-source(paste(getwd(), "my_apiKeys.R", sep = "/"))
+source(paste(getwd(), "scripts/my_apiKeys.R", sep = "/"))
 
 # Set FRED API Key
 fredr_set_key(my_fredKey)
-
