8852440d7598790346fa3ef8d3bdacb4b7771e0f
ryanj-0 <johnson.ryan1019@gmail.com>
2025-10-21 23:04:45 -0600

    update formatting

--- scripts/importBEA_FDI.R
+++ scripts/importBEA_FDI.R
@@ -4,18 +4,25 @@
 ## - Foriegn Direct Investment
 #######################################################
 
-FDI_A <- beaGet(list('UserID' = my_beaKey,
-                     'Method' = 'GetData',
-                     'datasetname' = 'MNE',
-                     'DirectionOfInvestment' = 'Inward',
-                     'Classification' = 'Industry',
-                     'Year' = "All")) |>
-    filter(Column %in% c("First-year expenditures",
-                         "Planned total expenditures"),
-           Row == "All Industries Total") |>
-    pivot_longer(cols = !SeriesID:TableRowDisplayOrder,
-                 names_to = "time",
-                 values_to = "expenditures") |>
+FDI_A <- beaGet(
+    list(
+        'UserID' = my_beaKey,
+        'Method' = 'GetData',
+        'datasetname' = 'MNE',
+        'DirectionOfInvestment' = 'Inward',
+        'Classification' = 'Industry',
+        'Year' = "All"
+    )
+) |>
+    filter(
+        Column %in% c("First-year expenditures", "Planned total expenditures"),
+        Row == "All Industries Total"
+    ) |>
+    pivot_longer(
+        cols = !SeriesID:TableRowDisplayOrder,
+        names_to = "time",
+        values_to = "expenditures"
+        ) |>
     mutate(year = str_extract(time, "[0-9]{4}")) |>
     drop_na(expenditures) |>
     select(Row, Column, TableScale, year, expenditures)
--- scripts/importBEA_NIPA.R
+++ scripts/importBEA_NIPA.R
@@ -6,79 +6,117 @@
 ########################################################
 
 # Global config for NIPA Tables
-nipaConfig <- list('UserID' = my_beaKey,
-                  'Method' = 'GetData',
-                  'Year' = 'ALL',    # Get All Years Available
-                  'ResultFormat' = 'json')
+nipaConfig <- list(
+    'UserID' = my_beaKey,
+    'Method' = 'GetData',
+    # Get All Years Available
+    'Year' = 'ALL',
+    'ResultFormat' = 'json'
+)
 
 
 # GDP ---------------------------------------------------------------------
 
 # Annual GDP
-gdpA_list <- append(nipaConfig, list('datasetname' = 'NIPA',
-                                    'TableName' = 'T10101',
-                                    'Frequency' = 'A'))
+gdpA_list <- append(nipaConfig,
+                    list(
+                        'datasetname' = 'NIPA',
+                        'TableName' = 'T10101',
+                        'Frequency' = 'A'
+                    ))
 
 GDP_A <- beaGet(gdpA_list) |>
-    pivot_longer(cols = !(TableName:UNIT_MULT),
-                 names_to = "time",
-                 values_to = "pctChange") |>
-    mutate(year = str_extract(time, "[0-9]{4}"),
-           LineDescription = case_when(LineNumber == 17 ~ "ExportGoods",
-                                       LineNumber == 18 ~ "ExportServices",
-                                       LineNumber == 20 ~ "ImportGoods",
-                                       LineNumber == 21 ~ "ImportServices",
-                                       .default = LineDescription)) |>
-    select(LineDescription, CL_UNIT, year, pctChange) |>
-    pivot_wider(names_from = LineDescription,
-                values_from = pctChange)
+    pivot_longer(
+        cols = !(TableName:UNIT_MULT),
+        names_to = "time",
+        values_to = "pctChange"
+    ) |>
+    mutate(
+        year = str_extract(time, "[0-9]{4}"),
+        LineDescription = case_when(
+            LineNumber == 17 ~ "ExportGoods",
+            LineNumber == 18 ~ "ExportServices",
+            LineNumber == 20 ~ "ImportGoods",
+            LineNumber == 21 ~ "ImportServices",
+            .default = LineDescription
+        )
+    ) |>
+    select(LineDescription, year, pctChange) |>
+    pivot_wider(names_from = LineDescription, values_from = pctChange) |>
+    rename_with( ~ gsub(" ", "_", .x)) |>
+    rename_with( ~ paste0(.x, "_%"), .cols = -year)
 
 # GDP Quartly
-gdpQ_list <- append(nipaConfig, list('datasetname' = 'NIPA',
-                                    'TableName' = 'T10101',
-                                    'Frequency' = 'Q'))
+gdpQ_list <- append(nipaConfig,
+                    list(
+                        'datasetname' = 'NIPA',
+                        'TableName' = 'T10101',
+                        'Frequency' = 'Q'
+                    ))
+
 GDP_Q <- beaGet(gdpQ_list) |>
-    pivot_longer(cols = !(TableName:UNIT_MULT),
-                 names_to = "time",
-                 values_to = "pctChange") |>
-    mutate(year = str_extract(time, "[0-9]{4}",),
-           quarter = str_extract(time, "\\d$"),
-           LineDescription = case_when(LineNumber == 17 ~ "ExportGoods",
-                                       LineNumber == 18 ~ "ExportServices",
-                                       LineNumber == 20 ~ "ImportGoods",
-                                       LineNumber == 21 ~ "ImportServices",
-                                       .default = LineDescription)) |>
-    select(LineDescription, CL_UNIT, year, quarter, pctChange) |>
-    pivot_wider(names_from = LineDescription,
-                values_from = pctChange)
+    pivot_longer(
+        cols = !(TableName:UNIT_MULT),
+        names_to = "time",
+        values_to = "pctChange"
+    ) |>
+    mutate(
+        year = str_extract(time, "[0-9]{4}", ),
+        quarter = str_extract(time, "\\d$"),
+        LineDescription = case_when(
+            LineNumber == 17 ~ "ExportGoods",
+            LineNumber == 18 ~ "ExportServices",
+            LineNumber == 20 ~ "ImportGoods",
+            LineNumber == 21 ~ "ImportServices",
+            .default = LineDescription
+        )
+    ) |>
+    select(LineDescription, year, quarter, pctChange) |>
+    pivot_wider(names_from = LineDescription, values_from = pctChange) |>
+    rename_with( ~ gsub(" ", "_", .x)) |>
+    rename_with( ~ paste0(.x, "_%"), .cols = -c(year, quarter))
 
 
 # Personal Income ---------------------------------------------------------
 
 # Persional Income and Outlays Annual
-pidA_list <- append(nipaConfig, list('datasetname' = 'NIPA',
-                                     'TableName' = 'T20100',
-                                     'Frequency' = 'A'))
+pidA_list <- append(nipaConfig,
+                    list(
+                        'datasetname' = 'NIPA',
+                        'TableName' = 'T20100',
+                        'Frequency' = 'A'
+                    ))
+
 PID_A <- beaGet(pidA_list) |>
-    pivot_longer(cols = !(TableName:UNIT_MULT),
-                 names_to = "time",
-                 values_to = "dollars") |>
+    pivot_longer(
+        cols = !(TableName:UNIT_MULT),
+        names_to = "time",
+        values_to = "dollars"
+    ) |>
     mutate(year = str_extract(time, "[0-9]{4}")) |>
     select(LineDescription, year, dollars) |>
-    pivot_wider(names_from = LineDescription,
-                values_from = dollars)
+    pivot_wider(names_from = LineDescription, values_from = dollars) |>
+    rename_with( ~ gsub(" ", "_", .x)) |>
+    rename_with( ~ paste0(.x, "_M"), .cols = -year)
 
 
 # Persional Income and Outlays Quarterly
-pidQ_list <- append(nipaConfig, list('datasetname' = 'NIPA',
-                                     'TableName' = 'T20100',
-                                     'Frequency' = 'Q'))
+pidQ_list <- append(nipaConfig,
+                    list(
+                        'datasetname' = 'NIPA',
+                        'TableName' = 'T20100',
+                        'Frequency' = 'Q'
+                    ))
+
 PID_Q <- beaGet(pidQ_list) |>
-    pivot_longer(cols = !(TableName:UNIT_MULT),
-                 names_to = "time",
-                 values_to = "dollars") |>
+    pivot_longer(
+        cols = !(TableName:UNIT_MULT),
+        names_to = "time",
+        values_to = "dollars"
+    ) |>
     mutate(year = str_extract(time, "[0-9]{4}"),
            quarter = str_extract(time, "\\d$")) |>
     select(LineDescription, year, quarter, dollars) |>
-    pivot_wider(names_from = LineDescription,
-                values_from = dollars)
+    pivot_wider(names_from = LineDescription, values_from = dollars) |>
+    rename_with( ~ gsub(" ", "_", .x)) |>
+    rename_with( ~ paste0(.x, "_M"), .cols = -c(year, quarter))
