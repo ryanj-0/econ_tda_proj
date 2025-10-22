9e20bd28327f43e3839110d4998693bd923e882b
ryanj-0 <johnson.ryan1019@gmail.com>
2025-10-21 23:04:17 -0600

    update format

--- functions/changeToAnnually.R
+++ functions/changeToAnnually.R
@@ -1,12 +1,10 @@
-changeTOAnnually <-
-    function(df) {
+changeTOAnnually <- function(df) {
+    return_df <- df |>
+        group_by(year) |>
+        mutate(avgA = mean(avgQ)) |>
+        ungroup() |>
+        select(series_id, year, avgA) |>
+        unique()
 
-        return_df <- df |>
-            group_by(year) |>
-            mutate(avgA = mean(avgQ)) |>
-            ungroup() |>
-            select(series_id, year, avgA) |>
-            unique()
-
-        return(return_df)
-    }
+    return(return_df)
+}
