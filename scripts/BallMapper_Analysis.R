ce096b0c43d6a74c1fd1a89892e0cd63e141c60b
ryanj-0 <johnson.ryan1019@gmail.com>
2025-11-25 14:36:25 -0700

    finish narrowing down analysis cols

--- scripts/BallMapper_Analysis.R
+++ scripts/BallMapper_Analysis.R
@@ -16,7 +16,7 @@ dataSources <- c("PPI_All",
 
 analysisData <- all_annual[dataSources]
 
-# Mutate cols in `change_tbl` to calculate pct annual change
+# Mutate cols in `change_tbl` to calculate pct annual change using log diff.
 change_tbl <- c("PPI_All", "CPI", "housingStarts")
 analysisData <- map2(analysisData, names(analysisData),
                      ~ {
@@ -25,7 +25,7 @@ analysisData <- map2(analysisData, names(analysisData),
 
                              .x |>
                                  mutate(pctChange =
-                                            (avgA - lag(avgA))/lag(avgA)) |>
+                                            (log(avgA) - log(lag(avgA)))*100) |>
                                  rename_with(~ paste0(., "_", seriesID),
                                              c(avgA, pctChange)
                                  )
@@ -40,50 +40,46 @@ analysisData <- map2(analysisData, names(analysisData),
 analysisData[["PPI_All"]] <- analysisData[["PPI_All"]] |>
     select(year, pctChange_PPIACO)
 
-# Reduce Personal Income and it's Disposition cols. Example how to read:
-# Large category:
-#   - included in category but not keeping
-#   - another col included not keeping
-#   - etc.
-
-# Supplements to Wages and Salaries
-# - Employer contrib. pension and insurance
-# - employer contrib. for gov social insurance
-
-# Personal Income Receipts on assets
-# - Personal Interest Income
-# - Personal Dividend Income
-
-# Personal Current transfer receipts
-# - Govnment social benefits:
-#       - Social security
-#       - Medicare/Medicaid
-#       - Unemployment insurance/Veteran's benefits/other
-# - Other current transfers (e.g. in-kind donations)
-# - Less: contrib. for gov. social insurance
+# Reduce Personal Income and it's Disposition columns.
+# Since this is a large macroeconomic analysis we are going to try and
+# keep the large categories
+# - Personal Income
+# - Employee Compensation
+# - Proprietors' Income w/ Invtry. Val. ... (Small Businesses)
+# -
 
 analysisData[["PID"]] <- analysisData[["PID"]] |>
-    select(year, c(45:50, 54:57, 60, 70:72, 78)) |>
-    rename_with(.cols = starts_with("Less:_"),
-                .fn = ~str_remove(.x, "Less:_")) |>
-    rename_with(.cols = starts_with("Equals:_"),
-                .fn = ~str_remove(.x, "Equals:_"))
+    select(
+        year,
+        Personal_income_PID,
+        Compensation_of_employees_PID,
+        `Proprietors'_income_with_inventory_valuation_and_capital_consumption_adjustments_PID`,
+        Personal_current_transfer_receipts_PID
+        )
 
 # Reducing GDP cols for computational and analysis purposes
-
-# GDP, current dollars
+# Removing Current dollars col
 analysisData[["GDP"]] <- analysisData[["GDP"]] |>
-    select(-26)
+    select(
+        year,
+        Personal_consumption_expenditures_GDP,
+        Gross_private_domestic_investment_GDP,
+        Government_consumption_expenditures_and_gross_investment_GDP,
+        Exports_GDP,
+        Imports_GDP
+        )
 
 analysisData[["CPI"]] <- analysisData[["CPI"]] |>
     select(year, pctChange_CPIAUCSL)
 
 analysisData[["unemployment"]] <- analysisData[["unemployment"]] |>
     rename("unrate" = avgA) |>
-    select(year, unrate)
+    mutate(pctChange_UNRATE = unrate - lag(unrate)) |>
+    select(year, unrate, pctChange_UNRATE)
 
 analysisData[["FFR"]] <- analysisData[["FFR"]] |>
-    select(year, rate_FFR)
+    mutate(pctChange_FFR = rate_FFR - lag(rate_FFR)) |>
+    select(year, rate_FFR, pctChange_FFR)
 
 analysisData[["housingStarts"]] <- analysisData[["housingStarts"]] |>
     select(year, pctChange_HOUST)
@@ -93,6 +89,5 @@ analysisYears <- c(1960:2024)
 analysisData <- map(analysisData, ~ filter(.x, year %in% analysisYears))
 
 # Reduce and combine tables
-finalData <- reduce(analysisData, full_join, by = "year") |>
-    select(-starts_with(c("Less:", "")))
+finalData <- reduce(analysisData, full_join, by = "year")
 
