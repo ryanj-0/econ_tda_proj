3d99bc9e36c887142bb4ed5dfd0bd282f08ed31b
ryanj-0 <johnson.ryan1019@gmail.com>
2025-11-30 20:28:24 -0700

    take out graphing code

--- scripts/Method_EDA.R
+++ scripts/Method_EDA.R
@@ -20,68 +20,22 @@ coloring <- finalData |>
 
 # BallMapper Testing ------------------------------------------------------
 
-e = 0.6
+e = 0.537
 
-econ_bm <- BallMapper(points = pointcloud, values = coloring, epsilon = e)
+econ_bm <- BallMapper(points = pointcloud,
+                      values = coloring,
+                      epsilon = e)
+econ_igraph <- bm_to_igraph(econ_bm)
 
+# Old Graph
 ColorIgraphPlot(outputFromBallMapper = econ_bm)
 title(main = e)
 
+# New Graph
+bm_ggraph(econ_igraph, e)
 
-econ_igraph <- bm_to_igraph(econ_bm)
-
-investigate <- 17
+investigate <- 1
 reference_data[V(econ_igraph)$members[[investigate]], ]
-
-# Custom Graph ------------------------------------------------------------
-
-new_bm_graph <- function(bm_igraph_output, epsilon) {
-
-    ggraph(bm_igraph_output, layout = "kk") +
-        geom_edge_link(
-            aes(width = weight,
-                color = weight > 3
-            )
-        ) +
-        scale_edge_color_manual(
-            values = c("TRUE" = "#000000",
-                       "FALSE" = "#BEBEBE"),
-            guide = "none"
-        ) +
-        scale_edge_width(
-            range = c(0.2, 1),
-            guide = "none"
-        ) +
-        geom_node_point(
-            aes(
-                color = coloring,
-                size = size
-            )
-        ) +
-        geom_node_text(
-            aes(label = name)
-        ) +
-        scale_size_area(
-            max_size = 25,
-            guide = "none"
-        ) +
-        scale_color_gradientn(
-            name = coloring |> names() |> str_to_title(),
-            colors = c("#67A9CF", "#D1E5F0", "#F7F7F7", "#FDDBC7", "#EF8A62")
-        ) +
-        theme_void() +
-        labs(
-            title = "BallMapper Output",
-            subtitle = paste0("Epsilon: ", epsilon),
-            caption = paste0("N: ", nrow(pointcloud))
-        ) +
-        theme(
-            text = element_text(family = "EB Garamond"),
-            legend.position = "bottom",
-            legend.key.width = unit(0.1, "npc")
-
-        )
-}
 
 # Parallel Epsilon Search -------------------------------------------------
 library(future)
