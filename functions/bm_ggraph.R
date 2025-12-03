bcce283e5b901a4662d2d134434a14c06dc3cc71
ryanj-0 <johnson.ryan1019@gmail.com>
2025-12-03 14:52:45 -0700

    changing color palette

--- functions/bm_ggraph.R
+++ functions/bm_ggraph.R
@@ -14,7 +14,7 @@ bm_ggraph <- function(bm_igraph_output, epsilon) {
         ) +
         scale_edge_color_manual(
             values = c("TRUE" = "#000000",
-                       "FALSE" = "#BEBEBE"),
+                       "FALSE" = "#808080"),
             guide = "none"
         ) +
         scale_edge_width(
@@ -23,21 +23,24 @@ bm_ggraph <- function(bm_igraph_output, epsilon) {
         ) +
         geom_node_point(
             aes(
-                color = coloring,
+                fill = coloring,
                 size = size
-            )
+            ),
+            shape = 21,
+            color = "#000000",
+            stroke = 0.5
         ) +
         geom_node_text(
-            aes(label = name,
-                size = log(size))
+            aes(label = name
+            )
         ) +
         scale_size_area(
             max_size = 25,
             guide = "none"
         ) +
-        scale_color_gradientn(
+        scale_fill_gradientn(
             name = coloring |> names() |> str_to_title(),
-            colors = c("#67A9CF", "#D1E5F0", "#F7F7F7", "#FDDBC7", "#EF8A62")
+            colors = c("#0072B2", "#00B0E1", "#009B77", "#F0E442", "#E69F00")
         ) +
         labs(
             title = "BallMapper Output",
@@ -47,11 +50,7 @@ bm_ggraph <- function(bm_igraph_output, epsilon) {
         theme(
             text = element_text(family = "EB Garamond"),
             legend.position = "bottom",
-            legend.key.width = unit(0.1, "npc"),
-            legend.text = element_text(size = 16),
-            legend.title = element_text(size = 16),
-            plot.title = element_text(size = 20),
-            plot.subtitle = element_text(size = 18)
+            legend.key.width = unit(0.1, "npc")
 
         )
 }
