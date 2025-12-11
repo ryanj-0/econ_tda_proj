4ebb10114a5f845ecb99d3c9556ee7fbd72836ee
ryanj-0 <johnson.ryan1019@gmail.com>
2025-12-11 12:58:23 -0700

    moved parallel section

--- scripts/Exploratory.R
+++ scripts/Exploratory.R
@@ -43,3 +43,62 @@ walk2(
 )
 
 dev.off()
+
+# Further Analysis With Chosen Epsilon ------------------------------------
+
+coloring_vec <- final_data |>
+    select(-c(row_id)) |>
+    names()
+
+e = 0.511
+
+# Computing All Colorings
+library(future)
+library(furrr)
+
+econ_all_coloring <- function(coloring) {
+
+    # Set coloring
+    coloring <- final_data |>
+        select(all_of(coloring)) |>
+        as.data.frame()
+
+    # Run BallMapper
+    bm <- BallMapper(points = pointcloud, values = coloring, epsilon = e)
+    bm_final <- bm_to_igraph(bm)
+    return(bm_final)
+
+}
+
+# computing
+if(Sys.info()[["nodename"]] == "zenbook") {
+    plan(strategy = multisession,
+         workers = parallel::detectCores() - 1)
+} else {
+    plan(strategy = multisession,
+         workers = parallel::detectCores() - 2)
+}
+
+
+econ_all_bm <- future_map(
+    .x = coloring_vec,
+    .f = econ_all_coloring,
+    .options = furrr_options(seed = 2025))
+
+# parallel off
+plan(sequential)
+
+gc()
+
+# plot all bm graphs
+pdf(paste0("bm_all_coloring_00_", format(Sys.Date(), "%Y%m%d"), ".pdf"))
+walk2(
+    .x = test,
+    .y = coloring_vec,
+    .f = ~ {
+        p <- bm_ggraph(.x, .y)
+        print(p)
+    }
+)
+
+dev.off()
