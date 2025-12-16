8f7630eb3096a8c4d4950915a480122eabb2edf4
ryanj-0 <johnson.ryan1019@gmail.com>
2025-12-16 14:30:16 -0700

    move notes to script

--- scripts/Exploratory.R
+++ scripts/Exploratory.R
@@ -115,3 +115,36 @@ walk2(
 )
 
 dev.off()
+
+
+
+# Notes -------------------------------------------------------------------
+
+# 11/29/20205
+# - inital range 0.3 - 1.4
+# - ran seq to find interesting maps (bm_loop_00), found that .4-.75 seem to produce the most interesting graphs so we will run another sequence to see if there are any nuances we should be interested in (bm_loop_01)
+# - starting at 0.0447, maps have 3 components
+# - around 0.468, small components start to have more points included. We see this reflected in sizing between connected points
+#
+# 11/30/2025
+# - run new maps with new graph function
+# - 0.537 & 0.538 have a three node component?
+#     - 0.546 & 0.547 have 4 components?
+#     - 0.61-0.625 2 components but the shape changes b/c of points moving around
+# - 0.677-0.75 2 components and various combine of nodes
+# - Most Interesting Epsilon Values:
+#     -
+#
+#     12/1/2025
+# - Choosing handful of maps to see which to use for analysis
+# - c(0.468, 0.473, 0.478, 0.487, 0.511, 0.600, 0.605, 0.614, 0.642, 0.716)
+# - 0.475-0.482 stable graph, no change
+# - 0.495 start to see 2-simplex form, persists throughout
+# - 3-7-11 --> 3-6-10 -->
+#     - Noticing developing three big groups of nodes, three "phases" of economy?
+#     - talk about differnt shapes: 2-simplex, mickey mouse, spurs, lolipop holders,
+# - make edge strenth acording edge length? #discussion
+#     - later year v past year 2-simplex pattern?
+#
+#     12/2/2025
+# - Narrowing down more which graph for analysis
